ce9931db6000ea8eb5fe4c0e226c360b10f82d49
TheSicilian12 <mguidera314@gmail.com>
2023-05-16 16:27:50 -0400

    product_details

---
+++ app/models/product_details.py
@@ -0,0 +1,27 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Product_Detail(db.Model):
+    __tablename__ = 'product_details'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    desc = db.Column(db.String(1000))
+    # image_main
+    # image_1
+    # image_2
+    # image_3
+    # image_4
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    updated_at = db.Column(db.DateTime)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'desc': self.desc,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+
