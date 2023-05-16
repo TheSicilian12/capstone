5a06ef3dd1d134a96cff32182c48616e35115bc0
TheSicilian12 <mguidera314@gmail.com>
2023-05-16 18:11:12 -0400

    carts

--- app/seeds/carts.py
+++ app/seeds/carts.py
@@ -1,26 +1,32 @@
-# from app.models import db, Cart, environment, SCHEMA
-# from sqlalchemy.sql import text
+from app.models import db, Cart, environment, SCHEMA
+from sqlalchemy.sql import text
 
 
-# # Adds a demo user, you can add other users here if you want
-# def seed_carts():
-#     cart1 = Cart(
-#        total_price: 100)
+# Adds a demo user, you can add other users here if you want
+def seed_carts():
+    cart1 = Cart(
+        user_id=1)
+    cart2 = Cart(
+        user_id=2)
+    cart3 = Cart(
+        user_id=3)
 
-#     db.session.add(cart1)
-#     db.session.commit()
+    db.session.add(cart1)
+    db.session.add(cart2)
+    db.session.add(cart3)
+    db.session.commit()
 
 
-# # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
-# # have a built in function to do this. With postgres in production TRUNCATE
-# # removes all the data from the table, and RESET IDENTITY resets the auto
-# # incrementing primary key, CASCADE deletes any dependent entities.  With
-# # sqlite3 in development you need to instead use DELETE to remove all data and
-# # it will reset the primary keys for you as well.
-# def undo_products():
-#     if environment == "production":
-#         db.session.execute(f"TRUNCATE table {SCHEMA}.carts RESTART IDENTITY CASCADE;")
-#     else:
-#         db.session.execute(text("DELETE FROM carts"))
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_products():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.carts RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM carts"))
 
-#     db.session.commit()
+    db.session.commit()
