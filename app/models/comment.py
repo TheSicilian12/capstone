28379f83ee8572213a5edba9e49016f07f9c803c
TheSicilian12 <mguidera314@gmail.com>
2023-05-16 16:35:50 -0400

    edits and comment

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,21 +1,23 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
 
-class Product_Inventory(db.Model):
-    __tablename__ = 'product_inventories'
+class Comment(db.Model):
+    __tablename__ = 'comments'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    inventory = db.Column(db.Integer)
+    details = db.Column(db.String(1000))
+    rating = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
     updated_at = db.Column(db.DateTime)
 
     def to_dict(self):
         return {
             'id': self.id,
-            'desc': self.inventory,
+            'details': self.details,
+            'rating': self.rating,
             'created_at': self.created_at,
             'updated_at': self.updated_at
         }
