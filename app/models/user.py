ea038ea77afcdf46eaaaf793f619900b6b14b56e
TheSicilian12 <mguidera314@gmail.com>
2023-05-17 12:40:16 -0400

    adjusted model user for phone to have BIGINT

--- app/models/user.py
+++ app/models/user.py
@@ -13,7 +13,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     first_name = db.Column(db.String(255), nullable=False)
     last_name = db.Column(db.String(255), nullable=False)
-    phone = db.Column(db.Integer)
+    phone = db.Column(db.BIGINT)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
