0908531d16fa2a6fe0dcd8b5cac61741b21eb64a
TheSicilian12 <mguidera314@gmail.com>
2023-05-17 13:40:38 -0400

    starting on homepage

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,1 +1,16 @@
+import React, { useState } from 'react';
+import { useDispatch, useSel } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
 import './HomePage.css';
+
+export default function HomePage() {
+
+    return(
+        <div>
+            Hello
+        </div>
+    )
+
+
+}
