929b3f60e3ae5fa2f109df19c45b5f7742571cd3
TheSicilian12 <mguidera314@gmail.com>
2023-05-18 19:53:12 -0400

    basic delete modal set up

--- react-app/src/components/DeleteSingleProductModal/index.js
+++ react-app/src/components/DeleteSingleProductModal/index.js
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-import { login } from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import "./DeleteSingleProductModal.css";
@@ -7,33 +5,19 @@ import '../UniversalCSS.css'
 
 export default function DeleteSingleProductModal() {
   const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+
   const { closeModal } = useModal();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = await dispatch(login(email, password));
-    if (data) {
-      setErrors(data);
-    } else {
-        closeModal()
-    }
+  const deleter = async () => {
+    // await dispatch(deleteProduct(id))
+    closeModal()
   };
 
   return (
     <>
       <h1>Delete</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <button className="buttons-small" type="submit">Yes</button>
+        <button className="buttons-small" onClick={deleter}>Yes</button>
         <button className="buttons-small" onClick={() => closeModal()}>No</button>
-      </form>
     </>
   );
 }
