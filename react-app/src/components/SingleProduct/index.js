57152f201b8cd27028766371494a4ce609140801
TheSicilian12 <mguidera314@gmail.com>
2023-05-22 19:51:12 -0400

    single product edit button broken

--- react-app/src/components/SingleProduct/index.js
+++ react-app/src/components/SingleProduct/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 import DeleteSingleProductModal from '../DeleteSingleProductModal';
 
@@ -19,7 +19,7 @@ export default function SingleProduct() {
     const products = useSelector(state => state.products)
     const user = useSelector(state => state.session.user)
     const cart = useSelector(state => state.cart.carts)
-    // console.log("cart: ", typeof cart)
+    const history = useHistory()
 
     const productId = Number(useParams().productId)
 
@@ -37,32 +37,36 @@ export default function SingleProduct() {
     const cartId = cart?.id
     const userId = user.id
 
+    const editRedirect = () => {
+        history.push(`/products/${productId}/edit`)
+    }
+
     return (
         <div className="border-blue single-product-page-container">
             <div className="border-black single-product-container">
-                {/* <div className="border-blue single-product-image-container box">
-                    <StandardButtons text="edit" path={`/products/${productId}/edit`} />
-                    <OpenModalButton
-                        buttonText="Delete"
-                        className="buttons-small"
-                        modalComponent={<DeleteSingleProductModal productId={productId} />}
-                    />
-                </div> */}
                 <div className="border-black single-product-image-container">
-                    <div className="border-black single-prdocut-main-image">
+                    <div className="single-prdocut-main-image">
 
                     </div>
                 </div>
 
                 <div className="border-black single-product-info-container">
-                    <h1>{singleProduct.name}</h1>
+                    <div className="border-blue single-product-header-container">
+                        <h1>{singleProduct.name}</h1>
+                        <button OnClick={editRedirect()}>Edit</button>
+                        <OpenModalButton
+                            buttonText="Delete"
+                            className="buttons-small"
+                            modalComponent={<DeleteSingleProductModal productId={productId} />}
+                        />
+                    </div>
                     <p>{singleProduct.desc}</p>
                 </div>
 
                 <div className="border-black single-product-cart-container">
                     Add to cart
                     {products.product.inventory ? <div className="text-green">In Stock</div>
-                    : <div className="text-red">false</div>}
+                        : <div className="text-red">false</div>}
                     <AddItemCart cartId={cartId} userId={userId} productId={productId} />
                     <DeleteItemCart itemId={productId} />
                 </div>
