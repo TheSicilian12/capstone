2e977a5d517e845dbc592ea89ee63f26cb2b8a35
TheSicilian12 <mguidera314@gmail.com>
2023-05-27 12:36:37 -0400

    sign in

--- react-app/src/components/LoginFormPage/index.js
+++ react-app/src/components/LoginFormPage/index.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { login } from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 import shinanoLogoMini from "../assets/Images/ShinanoLogoSmall.jpg"
 import "./LoginForm.css";
 import '../UniversalCSS.css'
 
 function LoginFormPage() {
   const dispatch = useDispatch();
+  const history = useHistory();
   const sessionUser = useSelector((state) => state.session.user);
   const [email, setEmail] = useState("");
   const [disEmailErr, setDisEmailErr] = useState(false);
@@ -34,6 +35,9 @@ function LoginFormPage() {
   if (password.length < 8) err.password = "Password should be 8+ characters."
   const regEx = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
   if (!regEx.test(email)) err.email = "Please enter a valid email address."
+
+  let disableLogin = "button-disabled";
+  if (!Object.values(err).length > 0) disableLogin = "button-no-dimensions"
 
   return (
     <div className="login-container">
@@ -77,7 +81,7 @@ function LoginFormPage() {
           {disPassErr && <div className="errors">{err.password}</div>}
         </div>
         <button
-          className="login-page-button button-no-dimensions"
+          className={`login-page-button ${disableLogin}`}
           type="submit"
           disabled={Object.values(err).length > 0}>
           Log In
@@ -85,9 +89,19 @@ function LoginFormPage() {
         <button
           className="login-page-button button-no-dimensions"
           onClick={demoUserLogIn}>
-            Log in as a demo user
-          </button>
+          Log in as a demo user
+        </button>
       </form>
+
+      <div className="login-page-sign-up">
+        <p className="login-page-signup-text">New to Shinano?</p>
+        <button
+          className="login-page-button login-page-signup-button"
+          onClick={() => history.push("/signup")}>
+          Create your Shinano account
+        </button>
+      </div>
+
     </div>
   );
 }
