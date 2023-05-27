3ecd430daa2ababe1d12a928619d6336ee0bcac2
TheSicilian12 <mguidera314@gmail.com>
2023-05-26 20:07:09 -0400

    shopping cart logo now acts as the modal open

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import { deleteCartTHUNK, getItemsSingleCartTHUNK, getSingleCartTHUNK, postCartTHUNK } from '../../store/cart';
 import CartButton from '../CartButton';
 import shinanoLogoMini from "../assets/Images/ShinanoLogoSmall.jpg"
+import shinanoCart from "../assets/Images/Cart.jpg"
 
 import './Navigation.css';
 import '../UniversalCSS.css'
+import OpenCartModal from '../OpenCartModal';
 
 function Navigation({ isLoaded }) {
 	const dispatch = useDispatch()
@@ -18,6 +20,29 @@ function Navigation({ isLoaded }) {
 	const cart = useSelector(state => state.cart)
 
 	const [showMenu, setShowMenu] = useState(false);
+	const ulRef = useRef();
+
+	const openMenu = () => {
+		if (showMenu) return;
+		setShowMenu(true);
+	};
+
+	useEffect(() => {
+		if (!showMenu) return;
+
+		const closeMenu = (e) => {
+			if (!ulRef.current.contains(e.target)) {
+				setShowMenu(false);
+			}
+		};
+
+		document.addEventListener("click", closeMenu);
+
+		return () => document.removeEventListener("click", closeMenu);
+	}, [showMenu]);
+
+	const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+	// const closeMenu = () => setShowMenu(false);
 
 	let cartCheck = false;
 	// if (cart.carts.items === undefined) cartCheck = true
@@ -48,17 +73,33 @@ function Navigation({ isLoaded }) {
 
 	const closeMenu = () => setShowMenu(false);
 
+	const cartSideBar = () => {
+		openMenu()
+	}
+
+
 	return (
 		<div className="shinano-color-background nav-container">
-					<img
-						className="nav-logo"
-						onClick={() => history.push("/")}
-						src={shinanoLogoMini}/>
-		 	<div>
+			<img
+				className="nav-logo"
+				onClick={() => history.push("/")}
+				src={shinanoLogoMini} />
+			<div>
+				{sessionUser && cart["errors"] !== "No cart" && <CartButton cart={cart} itemNum={totalItems} />}
+				{sessionUser && cart["errors"] !== "No cart" && <img
+					className="nav-cart"
+					onClick={cartSideBar}
+					src={shinanoCart} />}
+				<ul className={ulClassName} ref={ulRef}>
+					<>
+						<OpenCartModal closeMenu={closeMenu} />
+					</>
+				</ul>
+
 				{isLoaded && <ProfileButton user={sessionUser} />}
 				{!sessionUser && <h2>You must be logged in to start a cart</h2>}
 				{sessionUser && cart["errors"] === "No cart" && <button onClick={addCart}>Start a cart</button>}
-				{sessionUser && cart["errors"] !== "No cart" && <CartButton cart={cart} itemNum={totalItems}/>}
+
 			</div>
 		</div>
 	);
