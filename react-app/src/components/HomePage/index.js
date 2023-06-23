5e30d01ba9b18f5bd8a899c6df72aa6d5f44510b
TheSicilian12 <mguidera314@gmail.com>
2023-06-23 17:34:18 -0400

    working on home page

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -21,24 +21,26 @@ export default function HomePage() {
     if (!products) return <div>loading homepage</div>
 
     return (
-        <div className="justify-center home-page">
+        <>
+            <div className="justify-center home-page">
                 <MainCarousel />
-            <div className="homepage-container">
-                {Object.values(products).map(product => {
-                    return (
-                        <div className="homepage-margin-product" key={product.id}>
-                            <ProductContainer product={product} />
-                        </div>
-                    )
-                })}
-            </div>
+                <div className="homepage-container">
+                    {Object.values(products).map(product => {
+                        return (
+                            <div className="homepage-margin-product" key={product.id}>
+                                <ProductContainer product={product} />
+                            </div>
+                        )
+                    })}
+                </div>
                 <div className="homepage-footer">
                     <div className="homepage-footer-item-container">
                         <a className="text-underline-none homepage-footer-item" href={"https://www.linkedin.com/in/michael-guidera-376214260/"} target="_blank" rel="noreferrer">Michael Guidera</a>
                         <a className="text-underline-none homepage-footer-item" href={"https://github.com/TheSicilian12/capstone"} target="_blank" rel="noreferrer">Shinano Repository</a>
                     </div>
                 </div>
-        </div>
+            </div>
+        </>
     )
 
 
