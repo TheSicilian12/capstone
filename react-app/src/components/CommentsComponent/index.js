72d93bcb3d41cdc784e5000ec09642a472aaaa76
TheSicilian12 <mguidera314@gmail.com>
2023-06-27 16:12:24 -0400

    comments display

--- react-app/src/components/CommentsComponent/index.js
+++ react-app/src/components/CommentsComponent/index.js
@@ -3,10 +3,13 @@ import React from 'react';
 import "./CommentsComponent.css"
 
 export default function CommentsComponent({comments}) {
+    console.log("comments: ", comments)
+
 
     return (
-       <div>
-        Hello........................................................................
-       </div>
+
+        <div>
+            {Object.values(comments).map((comment) => <div key={comment.id}>Comment: {comment.details}</div>)}
+        </div>
     )
 }
