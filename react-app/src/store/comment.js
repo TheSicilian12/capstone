c3d65a96cee067f53087fd6d211943de822e5c06
TheSicilian12 <mguidera314@gmail.com>
2023-06-28 12:03:57 -0400

    working on thunk

--- react-app/src/store/comment.js
+++ react-app/src/store/comment.js
@@ -21,7 +21,26 @@ export const getAllProductCommentsTHUNK = (productId) => async (dispatch) => {
 // Add a comment to a product THUNK
 export const addProductCommentTHUNK = (commentDetails) => async (dispatch) => {
     // Need details, rating, user_id, product_id
-    
+    const {details, rating, userId, productId} = commentDetails;
+
+    const payloadComment = {
+        details,
+        rating,
+        user_id: userId
+    }
+    const response = await fetch(`api/comments/product/${productId}/new`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(
+            payloadComment
+        )
+    })
+    if (response.ok) {
+        const data = await response.json()
+        return data
+    }
 
 }
 
