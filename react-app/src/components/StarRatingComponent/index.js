3424cf59364f18be8ed8873a507c75f48f3c066b
TheSicilian12 <mguidera314@gmail.com>
2023-06-28 14:36:30 -0400

    working on create a comment

--- react-app/src/components/StarRatingComponent/index.js
+++ react-app/src/components/StarRatingComponent/index.js
@@ -3,15 +3,24 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import "./StarRatingComponent.css"
 
-export default function StarRatingComponent({ groupId }) {
+export default function StarRatingComponent() {
     const dispatch = useDispatch();
+
+    const [rating, setRating] = useState(1)
+
+    const fullStar = "fa fa-star";
+    const emptyStar = "far fa-star";
+
+    // fullStar = <i className="fa fa-star"></i>;
+    // emptyStar =  <i className="far fa-star"></i>;
+    // halfStar = <i className="fa fa-star-half"></i>
 
     return (
         <div>
             Hello
-            <i className="fa fa-star"></i>
-            <i className="far fa-star"></i>
-            <i className="fa fa-star-half"></i>
+
+
+
         </div>
     )
 }
