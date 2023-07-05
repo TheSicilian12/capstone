4e3e68e2274a30e2a6885e76faceb19d53468647
TheSicilian12 <mguidera314@gmail.com>
2023-07-05 12:08:23 -0400

    working on search feature

--- react-app/src/components/SearchResultsPage/index.js
+++ react-app/src/components/SearchResultsPage/index.js
@@ -1,17 +1,28 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import ProductContainer from '../ProductContainer';
 import './SearchResultsPage.css';
 import '../UniversalCSS.css'
 
 export default function SearchResultsPage() {
     const dispatch = useDispatch();
-    const products = useSelector(state => state.products)
+    const searchProducts = useSelector(state => state.search.searchProducts)
     const user = useSelector(state => state.session.user)
 
+    if (!searchProducts) return <div>loading search page</div>
 
-    if (!products) return <div>loading search page</div>
+    console.log("search products: ", searchProducts)
 
     return (
-       <div>Hello</div>
+        <div>
+            <div>Hello</div>
+            {Object.values(searchProducts).map(product => {
+                return (
+                    <div className="" key={product.id}>
+                    <ProductContainer product={product} />
+                </div>
+                )
+            })}
+        </div>
     )
 }
