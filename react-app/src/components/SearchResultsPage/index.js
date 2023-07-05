a9fa710d67a1b04963d2e31bb22fe2c804957dc7
TheSicilian12 <mguidera314@gmail.com>
2023-07-05 12:25:57 -0400

    note

--- react-app/src/components/SearchResultsPage/index.js
+++ react-app/src/components/SearchResultsPage/index.js
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from "react-router-dom";
 import ProductContainer from '../ProductContainer';
 import './SearchResultsPage.css';
 import '../UniversalCSS.css'
 
 export default function SearchResultsPage() {
     const dispatch = useDispatch();
+    const history = useHistory();
     const searchProducts = useSelector(state => state.search.searchProducts)
     const user = useSelector(state => state.session.user)
 
     if (!searchProducts) return <div>loading search page</div>
 
     console.log("search products: ", searchProducts)
+
+    // The actual url needs to change for the search and that's what is searched so a refresh won't be a problem
 
     return (
         <div>
